0e66d1eeb19d781c0d412317f30a679c977cdf4c
PirompatA <pirompat.a@gmail.com>
2024-05-28 22:53:12 +0700

    restart project

--- entity/SummaryResponse.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-// Summary represents the summary response structure
-type SummaryResponse struct {
-	Province map[string]int `json:"Province"`
-	AgeGroup map[string]int `json:"AgeGroup"`
-}
